614f92dc4dd4f0c858cf88680d678dae09964408
jyyoon@ibleaders.co.kr <jyyoon@ibleaders.co.kr>
2022-07-04 10:49:10 +0900

    초기설정

---
+++ README.md
@@ -0,0 +1,6 @@
+sudo docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 \
+                   --restart=unless-stopped \
+                   -e RABBITMQ_DEFAULT_USER=root \
+                   -e RABBITMQ_DEFAULT_PASS=root \
+                   rabbitmq:management
+
---
+++ src/main/java/com/yoon/HowtoUseRabbitMq/HowtoUseRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.HowtoUseRabbitMq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtoUseRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowtoUseRabbitMqApplication.class, args);
+	}
+
+}
